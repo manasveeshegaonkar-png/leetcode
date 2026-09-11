05435d19f001cb842d8abb89e878289f333d55c0
manasveeshegaonkar-png <manasveeshegaonkar@gmail.com>
2026-09-12 01:30:50 +0530

    Time: 0 ms (100%), Space: 9.8 MB (93.9%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left=0;
+        int right=s.length()-1;
+        while(left<right){
+            //skip/ignore non-alphanumeric characters
+            while(left<right && !isalnum(s[left])){
+                left++;
+            }
+            while(left<right && !isalnum(s[right])){
+                right--;
+            }
+            // compare lowercase characters
+            if (tolower(s[left]) != tolower(s[right])){
+                return false;
+            }
+            left++,right--;
+        }
+        return true;
+        
+    }
+};
